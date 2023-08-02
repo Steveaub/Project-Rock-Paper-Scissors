77bfe1f9baf6f754bd67ff0a03ca303019dc15ab
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-08-01 21:06:10 -0400

    Add commits explaining task to be completed

---
+++ script.js
@@ -0,0 +1,9 @@
+// task- create a javascript program that will play rock paper scissors in against the computer.
+
+// this will be done  in console no UI needed
+// this user is going to need to pick rock, paper or scissors and the computer with also pick.
+// the rock paper and scissors need to be compared and a winner determined
+
+// define function That determines computer choice 
+// define function that determines user choice
+// compare computer and user choice than determine 
