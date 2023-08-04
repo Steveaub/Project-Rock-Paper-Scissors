f4f14113f486cbfcf45a47276cfeb84da1d826d5
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-08-03 23:34:15 -0400

    Refactor playRound function and add multiple round
     Moved the variable declarations for `playerSelection` and
     `computerSelection` inside the `playRound` function.
     This allows for the generation of different game results during each
     function call, thereby adding variety to the console game.
    
    2. Removed parameters from the `playRound` function to avoid
    syntax errors, as these were unnecessary and were causing conflicts
    with the internal variable declarations.
    
    This commit also introduces a new game function that plays
    5 random games of rock paper scissors.

--- script.js
+++ script.js
@@ -17,23 +17,48 @@ return choice [Math.floor(Math.random() * choice.length )];
 }
 
 console.log (getComputerChoice());
-console.log (getComputerChoice());
-console.log (getComputerChoice());
-console.log (getComputerChoice());
 
 
-function playRound(playerSelection, computerSelection) {
+
+function playRound(Playerselection) {
+  
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  
   playerSelection.toLowerCase();
   computerSelection.toLowerCase();
- if ( playerSelection === "rock" && computerSelection === "Rock") {
+  
+  if ( playerSelection === "Rock" && computerSelection === "Rock") {
         return "its a tie!"
     } else if ( playerSelection === "rock" && computerSelection === "Scissors") {
          return "You win! rock beats scissors"
     } else (playerSelection === "rock" && computerSelection === "Paper") 
          return "You lose! paper beats rock"
     
+         
   }
 
-  const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+console.log(playRound());
+
+
+function game() {
+  console.log( playRound() );
+  console.log( playRound() );
+  console.log( playRound() );
+  console.log( playRound() );
+  console.log( playRound() );
+
+  
+  
+}
+
+
+console.log(game());
+
+
+
+// const colors = [ "blue","green", "yellow", "red", "purple"];
+// // for (let color = 0; color < colors.length; color++ ) {
+// //   console.log(`${colors[color]} is my fav color`);
+// // }
