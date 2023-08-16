64eada4c3664f85e4e14d3a87909e183b954102f
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-08-15 21:12:01 -0400

    Create three buttons for rock, paper, scissors
    Used DOM so the user can have three buttons to click rock,paper,scissors
    vs writing in the prompt.

--- script.js
+++ script.js
@@ -1,83 +1,69 @@
 
 
 function getComputerChoice() {
-  const choice = ["Rock", "Paper", "scissors"]
-  return choice[Math.floor(Math.random() * choice.length)];
-
-}
-
-console.log(getComputerChoice())
-
-
-
-function playRound() {
-
-  let playerSelection = prompt("choose rock, paper, or scissors");
-  let computerSelection = getComputerChoice();
-
-  playerSelection = playerSelection.toLowerCase();
-  computerSelection = computerSelection.toLowerCase();
-
-  if (playerSelection === computerSelection) {
-    console.log("its a tie!");
-    return 0
-
-  } else if (playerSelection === "rock" && computerSelection === "paper" ||
-    playerSelection === "paper" && computerSelection === "scissors" ||
-    playerSelection === "scissors" && computerSelection === "rock") {
-
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
-    return -1
-
-  } else if (playerSelection === "rock" && computerSelection === "scissors" ||
-    playerSelection === "paper" && computerSelection === "rock" ||
-    playerSelection === "scissors" && computerSelection === "paper") {
-
-    console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-    return 1
-
-  } else {
-    return console.log ("Invalid Entry. Please chose rock, paper,scissors")
-
-  }
-
-}
-
-// console.log(playRound());
-
-
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    let result = playRound();
+    const choice = ["Rock", "Paper", "scissors"]
+    return choice[Math.floor(Math.random() * choice.length)];
   
-
-
-
-    if (result === 1) {
-      playerScore++
-
-    } else if (result === -1) {
-      computerScore++
-    } else if (typeof result !== "rock","paper","scissors") {
-     i--
+  }
+  
+  console.log(getComputerChoice())
+  
+  
+  
+  function playRound() {
+  
+    let playerSelection = "choose rock, paper, or scissors";
+    let computerSelection = getComputerChoice();
+  
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+  
+    if (playerSelection === computerSelection) {
+      console.log("its a tie!");
+      return 0
+  
+    } else if (playerSelection === "rock" && computerSelection === "paper" ||
+      playerSelection === "paper" && computerSelection === "scissors" ||
+      playerSelection === "scissors" && computerSelection === "rock") {
+  
+      console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+      return -1
+  
+    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+      playerSelection === "paper" && computerSelection === "rock" ||
+      playerSelection === "scissors" && computerSelection === "paper") {
+  
+      console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+      return 1
+  
+    } else {
+      return console.log ("Invalid Entry. Please chose rock, paper,scissors")
+  
     }
-
-    console.log(`Player: ${playerScore}, Computer: ${computerScore}`);
-
-
-    
+  
   }
+  
+  console.log(playRound());
+  
+  
+const body = document.querySelector("body")  
+const divForbuttons = document.createElement ("div");
 
-  if (playerScore > computerScore) {
-    return "Player wins!"
-  } else if (playerScore < computerScore) {
-    return "Computer Wins!"
-  } else {
-    return "ohh ohh Its a tie!"
-  }
+const rock = document.createElement("button");
+rock.textContent ="Rock";
 
-}
-console.log(game());
+const paper = document.createElement("button");
+paper.textContent = "paper";
+
+const scissors = document.createElement("button");
+scissors.textContent = "scissors";
+
+
+rock.addEventListener()
+
+
+divForbuttons.appendChild(rock);
+divForbuttons.appendChild(paper);
+divForbuttons.appendChild(scissors);
+
+body.appendChild(divForbuttons);
