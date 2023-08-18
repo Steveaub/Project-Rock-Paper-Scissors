fcefc0d2b04b923c88e42b14f230d2a512dd072f
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-08-18 00:31:37 -0400

    Refactor logic for rock,paper,scissor buttons
    this will allow players to use choose between rock paper scissors
    buttons instead on typing it in. a perameter was added in
    playRound(userChoice)

--- script.js
+++ script.js
@@ -1,71 +1,67 @@
 
 
 function getComputerChoice() {
-    const choice = ["Rock", "Paper", "scissors"]
-    return choice[Math.floor(Math.random() * choice.length)];
-  
+  const choice = ["Rock", "Paper", "scissors"]
+  return choice[Math.floor(Math.random() * choice.length)];
+
+}
+
+console.log(getComputerChoice())
+
+
+
+function playRound(userChoice) {
+
+  let playerSelection = userChoice;
+  let computerSelection = getComputerChoice();
+
+
+
+  if (playerSelection === computerSelection) {
+    result.textContent = "It's a tie!";
+    return 0
+
+  } else if (playerSelection === "rock" && computerSelection === "paper" ||
+    playerSelection === "paper" && computerSelection === "scissors" ||
+    playerSelection === "scissors" && computerSelection === "rock") {
+
+      result.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
+    return -1
+
+  } else if (playerSelection === "rock" && computerSelection === "scissors" ||
+    playerSelection === "paper" && computerSelection === "rock" ||
+    playerSelection === "scissors" && computerSelection === "paper") {
+
+      result.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
+
+  } else {
+    result.textContent = "Invalid Entry. Please chose rock, paper, or scissors.";
   }
-  
-  console.log(getComputerChoice())
-  
-  
-  
-  function playRound() {
-  
-    let playerSelection = "choose rock, paper, or scissors";
-    let computerSelection = getComputerChoice();
-  
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-  
-    if (playerSelection === computerSelection) {
-      console.log("its a tie!");
-      return 0
-  
-    } else if (playerSelection === "rock" && computerSelection === "paper" ||
-      playerSelection === "paper" && computerSelection === "scissors" ||
-      playerSelection === "scissors" && computerSelection === "rock") {
-  
-      console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
-      return -1
-  
-    } else if (playerSelection === "rock" && computerSelection === "scissors" ||
-      playerSelection === "paper" && computerSelection === "rock" ||
-      playerSelection === "scissors" && computerSelection === "paper") {
-  
-      console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
-      return 1
-  
-    } else {
-      return console.log ("Invalid Entry. Please chose rock, paper,scissors")
-  
-    }
-  
-  }
-  
-  console.log(playRound());
-  
-  
-const body = document.querySelector("body")  
-const divForbuttons = document.createElement ("div");
 
-const rock = document.createElement("button");
-rock.textContent ="Rock";
-
-const paper = document.createElement("button");
-paper.textContent = "paper";
-
-const scissors = document.createElement("button");
-scissors.textContent = "scissors";
+}
 
 
-rock.addEventListener("click",playRound);
-paper.addEventListener("click",playRound);
-scissors.addEventListener("click",playRound);
 
 
-divForbuttons.appendChild(rock);
-divForbuttons.appendChild(paper);
-divForbuttons.appendChild(scissors);
 
-body.appendChild(divForbuttons);
+
+
+
+
+const rock = document.getElementById("rock").addEventListener("click",()=> playRound("rock"));
+const paper = document.getElementById("paper").addEventListener("click",()=> playRound("paper"));
+const scissors = document.getElementById("scissors").addEventListener("click",()=> playRound("scissors"));
+
+
+// select/create parent
+// select/create child 
+// content 
+// append
+
+const result = document.getElementById("gameResults");
+
+
+
+
+
+
