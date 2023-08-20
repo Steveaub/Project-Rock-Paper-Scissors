fa1b4b21d187c9c926307a182f5a294c2bdb5746
Steve Aubourg <108376337+Steveaub@users.noreply.github.com>
2023-08-20 13:57:21 -0400

    Format code using Prettier

--- script.js
+++ script.js
@@ -1,115 +1,87 @@
-
-
 function getComputerChoice() {
-  const choice = ["rock", "paper", "scissors"]
+  const choice = ["rock", "paper", "scissors"];
   return choice[Math.floor(Math.random() * choice.length)];
-
 }
 
 let playerScore = 0;
-  let computerScore = 0;
-
-
+let computerScore = 0;
 
 function playRound(userChoice) {
-
-  
-
-
   let playerSelection = userChoice;
   let computerSelection = getComputerChoice();
 
- 
-
-  
   playerPoint.textContent = "Player Score: " + `${playerScore}`;
-;
   computerPoint.textContent = `Computer Score: ${computerScore}`;
 
-
-  if (computerScore ===5 || playerScore===5){
-    return 
+  if (computerScore === 5 || playerScore === 5) {
+    return;
   }
 
   if (playerSelection === computerSelection) {
-    roundResults.textContent = "its a tie!"
-    roundResults.className = 'animate__animated animate__fadeIn'; 
-
-  } else if (playerSelection === "rock" && computerSelection === "paper" ||
-    playerSelection === "paper" && computerSelection === "scissors" ||
-    playerSelection === "scissors" && computerSelection === "rock") {
-
+    roundResults.textContent = "its a tie!";
+    roundResults.className = "animate__animated animate__fadeIn";
+  } else if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
     roundResults.textContent = `You lose! ${computerSelection} beats ${playerSelection}.`;
-    roundResults.className = 'animate__animated animate__fadeIn'; 
-    computerScore++
+    roundResults.className = "animate__animated animate__fadeIn";
+    computerScore++;
     computerPoint.textContent = `Computer Score: ${computerScore}`;
-  
-  } else if (playerSelection === "rock" && computerSelection === "scissors" ||
-    playerSelection === "paper" && computerSelection === "rock" ||
-    playerSelection === "scissors" && computerSelection === "paper") {
-
-    roundResults.textContent =`You win! ${playerSelection} beats ${computerSelection}.`;
-    roundResults.className = 'animate__animated animate__fadeIn'; 
-    playerScore++ ;
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    roundResults.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
+    roundResults.className = "animate__animated animate__fadeIn";
+    playerScore++;
     playerPoint.textContent = "Player Score: " + `${playerScore}`;
-
-
-  
-  
-  } 
-
+  }
 
   if (playerScore >= 5 && computerScore < 5) {
-    winnerWinner.textContent = "Congratulations!!!!!! Player wins the game!"
+    winnerWinner.textContent = "Congratulations!!!!!! Player wins the game!";
     document.getElementById("rock").disabled = true;
     document.getElementById("paper").disabled = true;
     document.getElementById("scissors").disabled = true;
-  } else if ( computerScore >= 5){
-    winnerWinner.textContent = "Oh Spaghettios! You lose! Computer beats player."
+  } else if (computerScore >= 5) {
+    winnerWinner.textContent =
+      "Oh Spaghettios! You lose! Computer beats player.";
     document.getElementById("rock").disabled = true;
     document.getElementById("paper").disabled = true;
     document.getElementById("scissors").disabled = true;
   }
+}
 
-
-  }
-
-  function resetGame() {
-    playerScore = 0;
-    computerScore = 0;
-    playerPoint.textContent = `Player Score: ${playerScore}`;
-    computerPoint.textContent = `Computer Score: ${computerScore}`;
-    roundResults.textContent = "";
-    winnerWinner.textContent = "";
-    document.getElementById("rock").disabled = false;
-    document.getElementById("paper").disabled = false;
-    document.getElementById("scissors").disabled = false;
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  playerPoint.textContent = `Player Score: ${playerScore}`;
+  computerPoint.textContent = `Computer Score: ${computerScore}`;
+  roundResults.textContent = "";
+  winnerWinner.textContent = "";
+  document.getElementById("rock").disabled = false;
+  document.getElementById("paper").disabled = false;
+  document.getElementById("scissors").disabled = false;
 }
 
 document.getElementById("resetGame").addEventListener("click", resetGame);
 
-
-
-
-
 const roundResults = document.getElementById("roundResults");
 
-const winnerWinner = document.getElementById("gameWinner")
+const winnerWinner = document.getElementById("gameWinner");
 
 const gameScore = document.getElementById("gameScore");
 const computerPoint = document.getElementById("computerPoint");
 const playerPoint = document.getElementById("playerPoint");
 
-
-
-const rock = document.getElementById("rock").addEventListener("click",()=>playRound("rock"));
-const paper = document.getElementById("paper").addEventListener("click",()=>playRound("paper"));;
-const scissors = document.getElementById("scissors").addEventListener("click",()=>playRound("scissors"));;
-
-
-
-
-
-
-
-
+const rock = document
+  .getElementById("rock")
+  .addEventListener("click", () => playRound("rock"));
+const paper = document
+  .getElementById("paper")
+  .addEventListener("click", () => playRound("paper"));
+const scissors = document
+  .getElementById("scissors")
+  .addEventListener("click", () => playRound("scissors"));
